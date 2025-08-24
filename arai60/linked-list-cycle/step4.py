dd7b06a0b8417791bfd2f7b7b6c795a948d75f0c
katouyousuke <padyk165@gmail.com>
2025-08-24 14:30:07 +0900

    nodeのループ条件を厳格化

--- arai60/linked-list-cycle/step4.py
+++ arai60/linked-list-cycle/step4.py
@@ -8,3 +8,17 @@ class Solution(object):
             visited.add(node)
             node = node.next
         return False
+
+## 指摘反映
+
+class Solution(object):
+    def hasCycle(self, head):
+        visited = set()
+        node = head
+        while node is not None:
+            if node in visited:
+                return True
+            visited.add(node)
+            node = node.next
+        return False
+        
